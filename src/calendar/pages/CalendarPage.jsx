2efc5387d703ea222faf87f8a3613a8fcee0a564
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-04-27 15:18:23 -0500

    Escuchar Eventos

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,8 +2,9 @@ import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours } from 'date-fns';
-import { Navbar } from "../";
+import { Navbar, CalendarEvent } from "../";
 import { localizer, getMessagesES } from '../../helpers';
+import { useState } from 'react';
 
 
 
@@ -21,13 +22,14 @@ const events = [{
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
   const eventStyleGetter = (event, start, end, isSelected) => {
-    console.log({ event, start, end, isSelected });
 
     const style = {
-      backgroundColor : '#347CF7',
+      backgroundColor: '#347CF7',
       borderRadius: '0px',
-      opacity : 0.8,
+      opacity: 0.8,
       color: 'white'
     }
 
@@ -35,6 +37,23 @@ export const CalendarPage = () => {
       style
     }
   };
+
+
+  const onDoubleClick = (event) => {
+    console.log({
+      doubleClick: event
+    });
+  }
+  const onSelect = (event) => {
+    console.log({
+      click: event
+    });
+  }
+
+  const onViewChange = (event) => {
+    localStorage.setItem('lastView', event);
+  }
+
   return (
     <>
       <Navbar />
@@ -43,11 +62,18 @@ export const CalendarPage = () => {
         culture='es'
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
         messages={getMessagesES()}
         eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChange}
       />
     </>
   )
