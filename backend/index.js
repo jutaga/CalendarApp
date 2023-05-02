0842be25ce4b65cc06ad71eeb16333cf72cac24c
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-05-02 15:38:32 -0500

    Demo Backend App

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+
+
+
+// Crear el servidor de express
+const app = express();
+
+
+//Directorio Publico
+app.use(express.static('public'));
+
+
+//Rutas
+app.use('/api/auth/', require('./routes/auth'));
+//TODO: CRUD : eventos
+
+
+//Escuchar Peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+})
