fdb76d0a14b513eff09d67c2b11013a77b091cc5
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-05-03 14:22:22 -0500

    mongoose

---
+++ backend/database/config.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+
+    try {
+
+        await mongoose.connect(process.env.DB_CNN);
+
+        console.log('DB online');
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('error al inicializar la base de datos');
+    }
+}
+
+
+module.exports = {
+    dbConnection
+}
