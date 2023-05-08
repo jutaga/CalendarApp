2244cae72e4be3068f4b4886271c7c1cc37818e3
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-05-08 12:16:55 -0500

    Browser

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,16 +1,14 @@
 import { Provider } from "react-redux";
 import { AppRouter } from "./router/"
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from "./store/store";
 
 export const CalendarApp = () => {
   return (
     <Provider store={store} >
-      {/* <BrowserRouter> */}
-      <HashRouter>
+      <BrowserRouter>
         <AppRouter />
-      </HashRouter>
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     </Provider>
 
   )
